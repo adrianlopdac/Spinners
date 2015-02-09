5ae4078bfb3c3e529bb43e992f2d28bd6ea613a2
adrianld <adrianlopezdacosta@gmail.com>
2015-02-09 14:44:13 +0100

    Con ArrayList

--- app/src/main/java/com/example/adrian/spinners/MainActivity.java
+++ app/src/main/java/com/example/adrian/spinners/MainActivity.java
@@ -15,11 +15,16 @@ import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
     Spinner miSpinner;
     String[] misColores;
     Context miContexto;
+    ArrayList<String> coloresArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +32,21 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         miContexto = this;
         miSpinner = (Spinner)findViewById(R.id.spinnerColores);
-        misColores = getResources().getStringArray(R.array.colors);
+       cargarColores();
 
-
-       ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        miSpinner.setAdapter(adapter);
+             ArrayAdapter<String> miAdapter = new ArrayAdapter<String>(miContexto,android.R.layout.simple_spinner_item,coloresArray);
+        miAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        miSpinner.setAdapter(miAdapter);
 
 
 
        miSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                RelativeLayout miLayout = (RelativeLayout)findViewById(R.id.miLayout);
-                String miColor=misColores[position];
+               RelativeLayout miLayout = (RelativeLayout)findViewById(R.id.miLayout);
+                String miColor=coloresArray.get(position);
                 miLayout.setBackgroundColor(getResources().getColor(getResources().getIdentifier(miColor, "color", miContexto.getPackageName())));
-                
+
             }
 
             @Override
@@ -52,7 +56,18 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-
+    private void cargarColores(){
+        int i = 0;
+        TypedArray colores = getResources().obtainTypedArray(R.array.colors);
+        coloresArray = new ArrayList<String>();
+        while(i<colores.length()){
+            int aux = colores.getResourceId(i,-1);
+            coloresArray.add(getResources().getResourceEntryName(aux));
+            i++;
+        }
+       // Toast.makeText(this,coloresArray[i],Toast.LENGTH_LONG).show();
+      // coloresArray.toArray(misColores);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
